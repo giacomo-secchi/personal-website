8fd2b00472b37d7c544fab606ec32d0cff4552e3
Giacomo Secchi <giacomosecchi@gmail.com>
2026-01-03 22:13:15 +0100

    Add navigation links for Blog, Portfolio, Resume, and WritePoetry in header

--- themes/personal-theme/patterns/header-default.php
+++ themes/personal-theme/patterns/header-default.php
@@ -18,7 +18,18 @@
 			<!-- wp:site-title /-->
 		</div>
 		<!-- /wp:group -->
-		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true}} /-->
+		<?php
+			$blog_page = get_page_by_path( 'blog' );
+			$blog_id   = $blog_page ? $blog_page->ID : '';
+			$blog_url  = get_permalink( $blog_id ) ?: home_url( '/blog/' );
+		?>
+		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"ariaLabel":"<?php esc_attr_e( 'Main Menu', 'personal-website' ); ?>"} -->
+			<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Blog', 'personal-website' ); ?>","type":"page","id":<?php echo $blog_id ?>,"url":"<?php echo esc_url( $blog_url ); ?>","kind":"post-type"} /-->
+			<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Portfolio', 'personal-website' ); ?>","type":"portfolio","url":"<?php echo esc_url( get_post_type_archive_link( 'portfolio' ) ); ?>","kind":"post-type"} /-->
+			<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Resume', 'personal-website' ); ?>","opensInNewTab":true,"url":"https://resume.giacomosecchi.com/","kind":"custom"} /-->
+			<!-- wp:navigation-link {"label":"<?php esc_html_e( 'WritePoetry', 'personal-website' ); ?>","opensInNewTab":true,"url":"https://write-poetry.com/","kind":"custom"} /-->
+			<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Get in Touch', 'personal-website' ); ?>","type":"custom","url":"<?php echo esc_url( home_url( 'contact-me/' ) ); ?>","kind":"post-type","isTopLevelLink":true} /-->
+		<!-- /wp:navigation -->
 	</div>
 	<!-- /wp:group -->
 </div>
