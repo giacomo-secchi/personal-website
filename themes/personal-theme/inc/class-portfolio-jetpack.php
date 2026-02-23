06c7e71e0f9ec2ffccfb4769c929a378530af02f
Giacomo Secchi <giacomosecchi@gmail.com>
2026-02-23 12:19:05 +0100

    refactor: Streamline portfolio query handling by consolidating pre_get_posts logic

--- themes/personal-theme/inc/class-portfolio-jetpack.php
+++ themes/personal-theme/inc/class-portfolio-jetpack.php
@@ -28,93 +28,46 @@ if ( ! class_exists( 'Portfolio_Jetpack' ) ) :
 		 */
 		public function __construct() {
 
-			// add_action( 'wp_head', function() {
-			// 	if ( is_archive( 'jetpack-portfolio' ) ) {
-			// 		add_filter( 'wp_body_open',  array( $this, 'popup_template' ), 10, 2 );
-			// 	}
-			// } );
+		
 
 
-			// add_filter( 'render_block_core/read-more', array( $this, 'modify_readmore_blocks' ), 10, 2 );
-
-			// add_action( 'wp_ajax_get_project',  array( $this, 'get_project_data' ), 10, 2 );
-			// add_action( 'wp_ajax_nopriv_get_project',  array( $this, 'get_project_data' ), 10, 2 );
-
-			// add_filter( 'render_block_core/post-terms', array( $this, 'remove_taxonomy_links' ), 10, 2 );
-		}
-
-
-		function modify_readmore_blocks( $block_content, $block ) {
-			if ( isset( $block['blockName'] ) && 'core/read-more' === $block['blockName'] ) {
-				global $post;
-
-				if ( $post && isset( $post->ID ) ) {
-					// Add the custom class to the block content using the HTML API.
-					$processor = new WP_HTML_Tag_Processor( $block_content );
-
-					if ( $processor->next_tag( 'a' ) ) {
-						$processor->set_attribute( 'data-project-id', $post->ID );
-
-						return $processor->get_updated_html();
-					}
+			add_action( 'pre_get_posts', function ( $query ) {
+				if ( is_admin() || ! $query->is_main_query() ) {
+					return;
 				}
-			}
-
-			return $block_content;
-		}
-
-
-		public function remove_taxonomy_links( $block_content, $block ) {
-
-			if ( isset( $block['attrs']['term'] ) && 'jetpack-portfolio-tag' === $block['attrs']['term'] ) {
-				// Remove all tag links from the block content.
-				$processor = new \WP_HTML_Tag_Processor( $block_content );
-
-				while ( $processor->next_tag( 'a' ) ) {
-					$processor->remove_attribute( 'href' );
-					$processor->remove_attribute( 'rel' );
+				
+				if ( 
+					! is_post_type_archive( self::CUSTOM_POST_TYPE ) 
+					// || ( ! is_tax( self::CUSTOM_TAXONOMY_TYPE ) )
+				) {
+					return;
 				}
 
-        		$block_content = $processor->get_updated_html();
+				// limit to 10 items per page.
+				$query->set( 'posts_per_page', 10 );
+			
+				// Order by the 'project_year' meta field, treating it as a numeric value.
+				$query->set( 'meta_key', 'project_year' );
+				$query->set( 'orderby', 'meta_value_num' );
+				$query->set( 'order', 'DESC' );
 
-			}
+				// filter projects without year to the end of the list.
+				$meta_query = array(
+					'relation' => 'OR',
+					array(
+						'key'     => 'project_year',
+						'compare' => 'NOT EXISTS',
+						'type'    => 'NUMERIC',
+					),
+					array(
+						'key'     => 'project_year',
+						'compare' => 'EXISTS',
+						'type'    => 'NUMERIC',
+					),
+				);
 
-			// Return the updated HTML content
-			return $block_content;
-		}
-
-
-
-
-		/**
-		 * Add a custom template to the footer.
-		 *
-		 * @since 0.1.0
-		 */
-		public function popup_template() {
-			echo '
-                <div class="popup d-none" aria-hidden="true" role="dialog">
-                   	<button type="button" class="popup-close" data-dismiss="modal" aria-label="' . esc_html__( 'Close', 'portfolio' ) . '">
-						<span aria-hidden="true">×</span>
-					</button>
-					<div class="popup-content" aria-labelledby="popup-title"></div>
-                </div>';
-		}
-
-		function get_project_data() {
-			check_ajax_referer( 'popup_nonce', 'nonce' );
-
-			$project_id = intval( $_POST['project_id'] );
-			$project = get_post( $project_id );
-
-			if ( ! $project ) {
-				wp_send_json_error( ['message' => 'Project not found'], 404 );
-			}
-
-			wp_send_json_success( [
-				'title'   => get_the_title( $project ),
-				'content' => apply_filters( 'the_content', $project->post_content )
-			] );
+				$query->set( 'meta_query', $meta_query );
+			} );
 		}
 	}
 endif;
